36e9128c34dd84e347a5674870fd7679228780a5
quagghyyIU <luongquanghuy592@gmail.com>
2025-09-06 02:35:50 +0700

    Add theme support and dark mode toggle; refactor styles with CSS variables

--- quiz-react-app/src/App.js
+++ quiz-react-app/src/App.js
@@ -3,28 +3,37 @@ import './css/App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Quiz from './Quiz';
 import QuizList from './QuizList';
+import { ThemeProvider } from './ThemeContext';
+import DarkModeToggle from './components/DarkModeToggle';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <div className="App">
-        <header className="App-header">
-          <h1>Networking Quiz</h1>
-          <nav>
-            <Link to="/" className="nav-link">Quiz List</Link>
-          </nav>
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<QuizList />} />
-            <Route path="/quiz" element={<Quiz />} />
-          </Routes>
-        </main>
-        <footer className="App-footer">
-          <p>Test your knowledge!</p>
-        </footer>
-      </div>
-    </Router>
+    <ThemeProvider>
+      <Router basename={process.env.PUBLIC_URL}>
+        <div className="App">
+          <header className="App-header">
+            <div className="header-left">
+              <h1>Networking Quiz</h1>
+              <nav>
+                <Link to="/" className="nav-link">Quiz List</Link>
+              </nav>
+            </div>
+            <div className="header-right">
+              <DarkModeToggle />
+            </div>
+          </header>
+          <main>
+            <Routes>
+              <Route path="/" element={<QuizList />} />
+              <Route path="/quiz" element={<Quiz />} />
+            </Routes>
+          </main>
+          <footer className="App-footer">
+            <p>Test your knowledge!</p>
+          </footer>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
